3cd03cb449d114efcdb2a6f52f941deccef88ca4
ahmdljyn <54071339+ahmdljyn@users.noreply.github.com>
2020-03-23 10:54:14 +0100

    Add files via upload

--- __tests__/index.js
+++ __tests__/index.js
@@ -9,8 +9,16 @@ describe('Server Request', () => {
     expect(response.statusCode).toEqual(200)
     done();
   })
-})
+  test('should be 300', async done => {
+    const response = await request(app)
+  .get(`/api`)
+    expect(response.statusCode).toEqual(300)
+    alert("[{user: 'fake', password: 'fake'}]")
+    done();
 
+})
+}
+,)
 beforeAll(async (done) => {
   server = app.listen(3000, () => {
     global.agent = request.agent(server);
